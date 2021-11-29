6c787c092a7aa47e468305de3213acda2855084a
Laravel Daily <info@laraveldaily.com>
2021-11-29 14:25:32 +0000

    QuickAdminPanel automatic commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                'id'                 => 1,
+                'name'               => 'Admin',
+                'email'              => 'admin@admin.com',
+                'password'           => bcrypt('password'),
+                'remember_token'     => null,
+                'approved'           => 1,
+                'verified'           => 1,
+                'verified_at'        => '2021-11-29 14:22:29',
+                'two_factor_code'    => '',
+                'verification_token' => '',
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
